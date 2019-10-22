b2c9a2cf5fe848cdbaf0ae5d1793c8bc726a5b84
Romina Eras <56611788+rominaerve28@users.noreply.github.com>
2019-10-21 19:13:36 -0500

    Add files via upload

---
+++ Prestamo.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciounocancha
+{
+    public class Prestamo
+    {
+
+        
+
+        private String cancha;
+
+        public String Cancha
+        {
+            get { return cancha; }
+            set { cancha = value; }
+        }
+
+
+        private float costo;
+
+        public float Costo
+        {
+            get {
+                Console.WriteLine("Seleccione la cancha ");
+                Console.WriteLine("1. De Futbol");
+                Console.WriteLine("2. De Futbolito");
+                Console.WriteLine("3. De Indor");
+                Console.WriteLine("4. De Básquet");
+                int canch = int.Parse(Console.ReadLine());
+                while (canch < 1 && canch > 4)
+                {
+                    Console.WriteLine("La opción ingresada no es validad, vuelva a intentarlo.");
+                    canch = int.Parse(Console.ReadLine());
+                }
+
+
+                switch (canch)
+                {
+
+                    case 1:
+                        costo = 20;
+                        break;
+                    case 2:
+                        costo = 15;
+                        break;
+                    case 3:
+                        costo = 5;
+                        break;
+                    case 4:
+                        costo = 5;
+                        break;
+
+                }
+                return costo;
+
+            }
+        }
+
+        private int horas;
+
+        public int Horas
+        {
+            get { return horas; }
+            set { 
+               while (horas >= 3)
+                {
+                    throw new Exception("No se hace prestamo mayor a 3 horas");
+                    value = int.Parse(Console.ReadLine());
+
+                }
+                horas = value;
+            }
+        }
+
+        private float descuento;
+
+        public float Descuento
+        {
+            get { return descuento; }
+            set
+            {
+
+                if (value != 10 && value != 20)
+                {
+                    throw new Exception(" el valor no puede ser diferente a 10 o 20 ");
+                    value = int.Parse(Console.ReadLine());
+
+
+                }
+
+                descuento = value;
+
+            }
+        }
+
+
+
+        private float subtotal;
+
+        public float Subtotal
+        {
+            get { return subtotal = costo*horas; }
+        }
+
+        private float iva;
+
+        public float Iva
+        {
+
+            get
+            {
+                return iva= Subtotal * 0.12f;
+
+            }
+            
+        }
+
+
+
+        private float subtotal2;
+
+        public float Subtotal2
+        {
+            get {
+                return subtotal2 = subtotal - descuento;
+
+            }
+
+        }
+
+
+       
+
+        private float final;
+
+        public float Final
+        {
+           get {return final= subtotal2 + iva; }
+           
+        }
+
+
+        private float pagado;
+
+        public float Pagado
+        {
+            get { return pagado; }
+            set {
+                while ((final * 50) / 100 >= value)
+                {
+                    Console.WriteLine("El anticipo debe ser mayor al 50% del valor a pagar");
+                    Console.WriteLine("Reingrese el anticipo");
+                    value = int.Parse(Console.ReadLine());
+                }
+                pagado = value;
+            }
+        }
+
+        private float pendiente;
+
+        public float Pendiente
+        {
+            get { return pendiente=  final-pagado ; }
+         
+        }
+
+        private string estado;
+
+        public string Estado    
+        {
+            
+           get { 
+                if (pendiente==0 )
+                {
+                    Console.WriteLine("PAGADO");
+                } else 
+                {
+                    Console.WriteLine("PENDIENTE");
+                }
+
+
+
+                return estado; }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciounocancha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Subtotal, Descuento, IVA, Total Final, Valor Pagado y Saldo Pendiente. 
+            Prestamo prestamo = new Prestamo();
+        
+            Prestamo Prestamo = new Prestamo();
+            Console.WriteLine(Prestamo.Costo);
+            Console.WriteLine("Ingrese las horas que va a usar la cancha");
+            Prestamo.Horas = int.Parse(Console.ReadLine());
+            Console.WriteLine(Prestamo.Horas);
+            Console.WriteLine("Ingrese el descuento que desea aplicar");
+            Prestamo.Descuento = int.Parse(Console.ReadLine());
+            Console.WriteLine("El subtotal natural es:" +Prestamo.Subtotal);
+            Console.WriteLine("El descuento es:"+ Prestamo.Descuento);
+            Console.WriteLine("El subtotal con descuento es:"+ Prestamo.Subtotal2);
+            Console.WriteLine("El valor del iva es:"+ Prestamo.Iva);
+            Console.WriteLine("El total a pagar es:"+ Prestamo.Final);
+            Console.WriteLine("Ingresar el anticipo");
+            Prestamo.Pagado = int.Parse(Console.ReadLine());
+            Console.WriteLine("El valor pagado es:"+ Prestamo.Pagado);
+            
+            //Console.WriteLine("El valor pendiente es:" + Prestamo.Pendiente);
+
+           // Console.WriteLine("El estado de la compra es:" + Prestamo.Estado);
+            Console.ReadKey();
+        }
+      
+
+    }
+}
